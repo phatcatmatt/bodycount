118b307dab730be462d083f0a427ae28cb796e58
Matthew Nicholas <phatcatmatt@gmail.com>
2016-04-15 17:43:27 -0600

    added stats for a users selection of movies

--- app/maincontroller.js
+++ app/maincontroller.js
@@ -10,16 +10,11 @@ angular.module('movieDeathsApp')
 
   $scope.allMovies = null
   $scope.myMovies = [];
-  var rCount = 0;
+
 
   $scope.callData = function(){
     mainSvc.getMovies().then(function(response){
       $scope.allMovies = (response)
-      for(var i = 0; i < $scope.allMovies.length; i++){
-        if($scope.allMovies[i].MPAA_Rating === "G"){
-          rCount++;
-        }
-      }
     })
   }
   $scope.callData();
@@ -28,14 +23,50 @@ angular.module('movieDeathsApp')
   $scope.addMovie = function(movie){
     $scope.myMovies.push($scope.allMovies[movie]);
     $scope.myBodyCount = 0;
+    $scope.totalMinutes = 0;
+    $scope.countM = 0;
+    $scope.countR = 0;
+    $scope.countPG13 = 0;
+    $scope.countPG = 0;
+    $scope.countG = 0;
+    $scope.countUnrated = 0;
+
     for(var i = 0; i < $scope.myMovies.length; i++){
       $scope.myBodyCount += $scope.myMovies[i].Body_Count;
+      $scope.totalMinutes += $scope.myMovies[i].Length_Minutes; 
+      if($scope.myMovies[i].MPAA_Rating === "M"){
+        $scope.countM++
+      }
+      else if($scope.myMovies[i].MPAA_Rating === "R"){
+        $scope.countR++
+      }
+      else if($scope.myMovies[i].MPAA_Rating === "PG-13"){
+        $scope.countPG13++
+      }
+      else if($scope.myMovies[i].MPAA_Rating === "PG"){
+        $scope.countPG++
+      }
+      else if($scope.myMovies[i].MPAA_Rating === "G"){
+        $scope.countG++
+      }
+      else if($scope.myMovies[i].MPAA_Rating === "Unrated"){
+        $scope.countUnrated++
+      }
     }
+    $scope.deathsPerMinute = Math.round(($scope.myBodyCount / $scope.totalMinutes) * 100) / 100
   }
 
   $scope.reset = function(){
     $scope.myMovies = [];
     $scope.myBodyCount = 0;
+    $scope.deathsPerMinute = 0;
+    $scope.totalMinutes = 0;
+    $scope.countM = 0;
+    $scope.countR = 0;
+    $scope.countPG13 = 0;
+    $scope.countPG = 0;
+    $scope.countG = 0;
+    $scope.countUnrated = 0;
   }
 
 
