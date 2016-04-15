c94ef558ac24ce83bbae26607a5a9c60f6a2e982
Matthew Nicholas <phatcatmatt@gmail.com>
2016-04-14 23:29:34 -0600

    added r ratings function

--- app/maincontroller.js
+++ app/maincontroller.js
@@ -1,15 +1,23 @@
 angular.module('movieDeathsApp').controller('mainCtrl', function($scope, mainSvc){
 
+$scope.allMovies = null
+
   $scope.callData = function(){
     mainSvc.getMovies().then(function(response){
-      $scope.allMovies = response;
+      $scope.allMovies = (response)
+      var rCount = 0;
+      for(var i = 0; i < $scope.allMovies.length; i++){
+        if($scope.allMovies[i].MPAA_Rating === "G"){
+          rCount++;
+        }
+      }
+      console.log(rCount)
+
     })
   }
   $scope.callData();
 
 
-  var clickTitle = function(){
-    !$scope.showCount;
-  }
+
 
 })
