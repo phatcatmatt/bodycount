d5a430e2472607d126bab022f5c580ddbe3ae94e
Matthew Nicholas <phatcatmatt@gmail.com>
2016-04-30 20:42:39 -0600

    cleaned up code and fixed a few bugs

--- app/app.js
+++ app/app.js
@@ -2,23 +2,24 @@ angular.module('movieDeathsApp', ['ui.router', 'firebase'])
     .constant('firebaseRoot', 'https://moviedeaths.firebaseio.com/')
     .config(function($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise('/')
+        $urlRouterProvider.otherwise('/');
 
         $stateProvider
             .state('my-data', {
                 url: '/my-data',
-                templateUrl: './app/mydata/myDataTmpl.html',
+                templateUrl: './app/mydata/myDataTmpl.html'
             })
 
             .state('my-list', {
                 url: '/my-list',
-                templateUrl: './app/mylist/myListTmpl.html',
+                templateUrl: './app/mylist/myListTmpl.html'
             })
 
             .state('all-movies', {
                 url: '/',
-                templateUrl: './app/allmovies/allMoviesTmpl.html',
+                templateUrl: './app/allmovies/allMoviesTmpl.html'
 
-            })
+            });
 
-    })
+    });
+  
--- app/maincontroller.js
+++ app/maincontroller.js
@@ -2,6 +2,8 @@ angular.module('movieDeathsApp')
     .controller('mainCtrl', function($scope, mainSvc, $firebaseObject,
         $firebaseArray, $firebaseAuth, firebaseRoot) {
 
+// connect with firebase to get all movie data and initiate lists
+
         var ref = new Firebase(firebaseRoot + 'moviedata/');
         $scope.allMovies = $firebaseArray(ref)
 
@@ -9,7 +11,15 @@ angular.module('movieDeathsApp')
         $scope.queryBy = '$';
         $scope.removedMovies = []
 
-        $scope.startCounters = function() {
+        $scope.addMovie = function(movie) {
+            $scope.myMovies.push(movie);
+            $scope.runCounters();
+          }
+
+// runs every time the user's list is changed to update stats (deaths per minute, ratings totals, total deaths, etc)
+
+          $scope.runCounters = function(){
+
             $scope.myBodyCount = 0;
             $scope.deathsPerMinute = 0;
             $scope.totalMinutes = 0;
@@ -33,72 +43,90 @@ angular.module('movieDeathsApp')
             $scope.myDataCountApproved = false;
             $scope.myDataCountGP = false;
             $scope.noMoviesMessage = false;
-        }
-
-        $scope.addMovie = function(movie) {
-
-            $scope.startCounters();
-            $scope.myMovies.push(movie);
 
             for (var i = 0; i < $scope.myMovies.length; i++) {
+
                 $scope.myBodyCount += $scope.myMovies[i].Body_Count;
                 $scope.totalMinutes += $scope.myMovies[i].Length_Minutes;
+                $scope.myMovies[i].deathsPerMinute = Math.round(($scope.myMovies[i].Body_Count / $scope.myMovies[i].Length_Minutes) * 100) / 100;
+                $scope.totalDeathsPerMinute = Math.round(($scope.myBodyCount / $scope.totalMinutes) * 100) / 100;
+                $scope.totalMovies = $scope.myMovies.length;
+                $scope.noMoviesMessage = true;
 
-                if ($scope.myMovies[i].MPAA_Rating === "M") {
-                    $scope.countM++
+                switch ($scope.myMovies[i].MPAA_Rating) {
+                  case "M":
+                    $scope.countM++;
                     $scope.myDataCountM = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "R") {
-                    $scope.countR++
-                    $scope.myDataCountR = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "PG-13") {
-                    $scope.countPG13++
-                    $scope.myDataCountPG13 = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "PG") {
-                    $scope.countPG++
-                    $scope.myDataCountPG = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "G") {
-                    $scope.countG++
-                    $scope.myDataCountG = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "Unrated") {
-                    $scope.countUnrated++
-                    $scope.myDataCountUnrated = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "X") {
-                    $scope.countX++
-                    $scope.myDataCountX = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "GP") {
-                    $scope.countGP++
-                    $scope.myDataCountGP = true;
-                } else if ($scope.myMovies[i].MPAA_Rating === "Approved") {
-                    $scope.countApproved++
-                    $scope.myDataCountApproved = true;
-                }
-                $scope.myMovies[i].deathsPerMinute = Math.round(($scope.myMovies[i].Body_Count / $scope.myMovies[i].Length_Minutes) * 100) / 100
-            }
-            $scope.totalDeathsPerMinute = Math.round(($scope.myBodyCount / $scope.totalMinutes) * 100) / 100
+                    break;
 
-            $scope.totalMovies = $scope.myMovies.length;
-            $scope.noMoviesMessage = true;
+                  case "R":
+                    $scope.countR++;
+                    $scope.myDataCountR = true;
+                    break;
+
+                  case "PG-13":
+                    $scope.countPG13++;
+                    $scope.myDataCountPG13 = true;
+                    break;
+
+                  case "PG":
+                    $scope.countPG++;
+                    $scope.myDataCountPG = true;
+                    break;
+
+                  case "G":
+                    $scope.countG++;
+                    $scope.myDataCountG = true;
+                    break;
+
+                  case "Unrated":
+                    $scope.countUnrated++;
+                    $scope.myDataCountUnrated = true;
+                    break;
+
+                  case "X":
+                    $scope.countX++;
+                    $scope.myDataCountX = true;
+                    break;
+
+                  case "GP":
+                    $scope.countGP++;
+                    $scope.myDataCountGP = true;
+                    break;
+
+                  case "Approved":
+                    $scope.countApproved++;
+                    $scope.myDataCountApproved = true;
+                    break;
+
+                }
+            }
         }
 
+
+// functions for resetting a user's list, removing and restoring movies to a user's list, and enabling and disabling adding movies
         $scope.reset = function() {
             for (var i = 0; i < $scope.myMovies.length; i++) {
               $scope.movieEnable($scope.myMovies[i])
             }
             $scope.myMovies = [];
-            $scope.startCounters();
-            $scope.removedMovies = []
+            $scope.removedMovies = [];
+            $scope.runCounters();
 
         }
 
         $scope.removeMovie = function(movie){
           $scope.removedMovies.push($scope.myMovies[movie])
           $scope.myMovies.splice([movie], 1);
+          $scope.runCounters();
         }
 
         $scope.restoreMovie = function(movie){
           $scope.myMovies.push($scope.removedMovies[movie])
           $scope.removedMovies.splice([movie], 1);
+          $scope.runCounters();
         }
+
 
         $scope.movieDisable = function(film){
           film.disabled = true;
@@ -107,5 +135,5 @@ angular.module('movieDeathsApp')
         $scope.movieEnable = function(film){
           film.disabled = false;
         }
-        
+
     })
